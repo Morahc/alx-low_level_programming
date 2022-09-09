0e1a78e5585ac3cd542c3f18a1f4cee6d7f3daa0
Morah Stephen <55032812+Morahc@users.noreply.github.com>
2022-09-09 20:32:08 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,16 +4,14 @@
 * main - prints lowercase alphabets in reverse 
 * Returns: 0 on succes
 */
-
 int main(void)
 {
-        char ch;
-        
-        for(ch = 'z'; ch >= 'a'; ch--)
-        {
-                putchar(ch);
-        }
-        putchar('\n');
-        return (0);
-}
+	char ch;
 
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
