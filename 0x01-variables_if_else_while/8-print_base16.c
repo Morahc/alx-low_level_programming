6e797c846f19946e07751130b1e6bdcfe94242ff
Morah Stephen <55032812+Morahc@users.noreply.github.com>
2022-09-09 20:27:31 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 	int i;
 	char ch;
 	
-        for(i = 48; i < 58; i++)
+        for (i = 48; i < 58; i++)
         {
-                putchar(i);
+        	putchar(i);
         }
-	for(ch = 'a'; ch <= 'f'; ch++)
+	for (ch = 'a'; ch <= 'f'; ch++)
 	{
 		putchar(ch);
 	}
