11beb65bf358ce539125845afa4d90a7c0c34a1c
Morah Stephen <55032812+Morahc@users.noreply.github.com>
2022-09-13 21:31:41 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,12 @@
 #include "main.h"
+#include <inttypes.h>
 
 /**
  * _abs - computes the absolute value of an integer
- *
+ * @i: absolute of number
  * Return: Always 1 on negative and 0 on positive
  */
 int _abs(int i)
 {
-	if (i >= 0)
-		return (0);
-	else
-		return (1);
+	return (abs(i));
 }
