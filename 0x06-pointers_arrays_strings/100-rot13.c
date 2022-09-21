e3eef7f105a6e04df54522c1a1c912c939e889b1
Morah Stephen <55032812+Morahc@users.noreply.github.com>
2022-09-21 22:45:51 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,28 +2,28 @@
 
 /**
  * rot13 - encodes a string in rot13
- * @c: string to be encoded
+ * @s: string to be encoded
  *
  * Return: the resulting string
  */
-char *rot13(char *c)
+char *rot13(char *s)
 {
 	int i, j;
 
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; c[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; a[j] != '\0'; j++)
 		{
-			if (c[i] == a[j])
+			if (s[i] == a[j])
 			{
-				c[i] = b[j];
+				s[i] = b[j];
 				break;
 			}
 		}
 	}
 
-	return (c);
+	return (s);
 }
