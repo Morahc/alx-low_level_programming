5996953f0ddcd469ecbc2070d9216a39132bbacf
Morah Stephen <55032812+Morahc@users.noreply.github.com>
2022-09-28 22:36:00 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -27,8 +27,11 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	if (*s1 == '\0' || *s2 == '\0')
+	{
 		return (0);
-
+	}
+	if (*s2 == '*')
+	{
 		s2 = move(s2);
 		if (*s2 == '\0')
 			return (1);
