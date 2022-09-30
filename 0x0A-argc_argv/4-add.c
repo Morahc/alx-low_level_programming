68848bdbf299ad16e2b8c45ad991bf054142e80c
Morah Stephen <55032812+Morahc@users.noreply.github.com>
2022-09-30 20:03:39 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,43 +1,56 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 /**
- * main - adds numbers
+ * StringCheck - checks string
+ * @s: string to check
+ * Return: boolean
+ */
+int StringCheck(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (!isdigit(s[i]))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+/**
+ * main - main function
  * @argc: argument count
  * @argv: array of arguments
- *
- * Return: 0 on success, otherwise 1
+ *Return: always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char  *argv[])
 {
+	int i;
+	int result = 0;
+
 	if (argc > 1)
 	{
-		int sum = 0;
-		int i;
-
 		for (i = 1; i < argc; i++)
 		{
-			if (isdigit(atoi(argv[i])) == 0)
+			if (StringCheck(argv[i]))
 			{
-				printf("Error\n");
-
-				return (1);
+				result += atoi(argv[i]);
 			}
 			else
 			{
-				sum += atoi(argv[i]);
+				printf("Error\n");
+				return (1);
 			}
 		}
-
-		printf("%d\n", sum);
-
+		printf("%d\n", result);
 		return (0);
 	}
 	else
 	{
 		printf("%d\n", 0);
-
 		return (1);
 	}
+
 }
