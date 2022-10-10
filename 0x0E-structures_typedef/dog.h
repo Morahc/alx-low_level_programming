625fea28e5eba72fa1c7155f75b798290331b19f
Morah Stephen <55032812+Morahc@users.noreply.github.com>
2022-10-10 19:00:31 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef _DOG_H_
+#define _DOG_H_
 /**
  * struct dog - a dog elements
  * @name: first memnber
@@ -20,3 +22,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+#endif
