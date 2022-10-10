093a21673a0ce84a4c306346987170046ca3556e
Morah Stephen <55032812+Morahc@users.noreply.github.com>
2022-10-10 19:04:43 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+
 /**
- * struct dog - a dog elements
- * @name: first memnber
- * @age: second member
- * @owner: third member
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
  */
 struct dog
 {
@@ -12,11 +21,6 @@ struct dog
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - typedef for struct dog
- */
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
