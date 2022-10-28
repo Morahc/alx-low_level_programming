7c5749892de194d8ddc37693c1e9f8c5787886e0
Morah Stephen <55032812+Morahc@users.noreply.github.com>
2022-10-28 07:48:23 +0100

    Update 0-binary_to_unit.c

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,55 +1,30 @@
 #include "main.h"
 
 /**
- * _pow - Computes x raised to power of y.
- * @x: The mantisse.
- * @y: The exposant.
- * Return: x ^ y.
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
+ *
+ * Return: unsigned int with decimal value of binary number, or 0 if error
  */
-int _pow(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	else if (y == 0)
-		return (1);
-	else if (y == 1)
-		return (x);
-	return (x * _pow(x, y - 1));
-}
 
-
-/**
- * _strlen - Returns the length of a string.
- * @s: The string whose the length should be returned.
- * Return: The length of s.
- */
-int _strlen(const char *s)
-{
-	if (*s == 0)
-		return (0);
-	else
-		return (1 + _strlen(s + 1));
-}
-
-
-
-/**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: the binary to convert.
- * Return: The unsigned int representation of the binary number.
- */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
-	int i = 0, n;
+	int i;
+	unsigned int n;
 
-	n = _strlen(b);
+	n = 0;
 	if (!b)
 		return (0);
-	for (i = 0; i < n; i++)
-		if (b[i] == '1')
-			res += _pow(2, n - 1 - i);
-		else if (b[i] != '0')
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-	return (res);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		n <<= 1;
+		if (b[i] == '1')
+			n += 1;
+	}
+	return (n);
 }
